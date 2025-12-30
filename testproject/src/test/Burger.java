fefa61b75e84f35c66c30e354f82d38dc5e62782
knuser <knuser@DESKTOP-CVRTY2P>
2025-12-30 15:45:23 +0900

    burger

--- testproject/src/test/Burger.java
+++ testproject/src/test/Burger.java
@@ -8,7 +8,7 @@ public class Burger {
 		System.out.println("양상추치즈피클양파");
 		System.out.println("특별한소스");
 		System.out.println("순쇠고기패티두장");
-		System.out.println("참깨빵");
+		System.out.println("빵");
 	}
 
 }
