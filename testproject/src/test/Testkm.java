a2b08438f3aa845c209a136a6268df62138d44cc
knuser <knuser@DESKTOP-OBNHTRC>
2025-12-30 15:35:52 +0900

    1111_ 더하기 빼기 곱하기 경민

---
+++ testproject/src/test/Testkm.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Testkm {
+public int aaa(int a, int b) {
+	return a+b;
+}
+public int bbb(int a, int b) {
+	return a-b;
+}
+public int ccc(int a, int b) {
+	return a*b;
+}
+}
