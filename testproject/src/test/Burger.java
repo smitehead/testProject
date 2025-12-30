0349e0426d7027de7e21ce6382b8d8bd16bf7f1c
knuser <knuser@DESKTOP-CVRTY2P>
2025-12-30 15:33:15 +0900

    Bigmac

---
+++ testproject/src/test/Burger.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Burger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("참깨빵");
+		System.out.println("양상추치즈피클양파");
+		System.out.println("특별한소스");
+		System.out.println("순쇠고기패티두장");
+		System.out.println("참깨빵");
+	}
+
+}
